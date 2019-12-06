d1c8079dbc0c534302bdaf05bbc89f2cbfade906
刘恺 <6678979@qq.com>
2019-12-06 15:25:22 +0800

    no message

--- src/main/java/com/lk/es/repository/ItemRepository.java
+++ src/main/java/com/lk/es/repository/ItemRepository.java
@@ -3,6 +3,15 @@ package com.lk.es.repository;
 import com.lk.es.entity.po.Item;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends ElasticsearchRepository<Item,Long> {
 
+    /**
+     * @Description:根据价格区间查询
+     * @Param price1
+     * @Param price2
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    List<Item> findByPriceBetween(double price1, double price2);
 }
